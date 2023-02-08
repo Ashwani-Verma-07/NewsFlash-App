42bcbfe8e3df25dd92ed2a36198f298f30445663
Ashwani Verma <vashwani0024@gmail.com>
2023-02-08 16:16:09 +0530

    Function based components

--- src/App.js
+++ src/App.js
@@ -1,154 +1,145 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import News from "./components/News";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
-export default class App extends Component {
-  pageSize = 20;
-  country = "in";
-  apiKey = process.env.REACT_APP_NEWS_API;
+const App = () => {
+  const pageSize = 20;
+  const country = "in";
+  const apiKey = process.env.REACT_APP_NEWS_API;
 
-  state = {
-    progress: 0,
-  };
-  setProgress = (progress) => {
-    this.setState({ progress: progress });
-  };
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navbar />
-          <LoadingBar
-            color="#f11946"
-            height={3}
-            progress={this.state.progress}
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <LoadingBar color="#f11946" height={3} progress={progress} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                key="generall"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="general"
+              />
+            }
           />
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="generall"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="general"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/business"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="business"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="business"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/entertainment"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="entertainment"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="entertainment"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/general"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="general"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="general"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/health"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="health"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="health"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/science"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="science"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="science"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/sports"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="sports"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="sports"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/technology"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="technology"
-                  image={URL}
-                  apiKey={this.apiKey}
-                  pageSize={this.pageSize}
-                  country={this.country}
-                  category="technology"
-                />
-              }
-            />
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
-}
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={setProgress}
+                key="business"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="business"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                key="entertainment"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/general"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="general"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                setProgress={setProgress}
+                key="health"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="health"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                setProgress={setProgress}
+                key="science"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="science"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                setProgress={setProgress}
+                key="sports"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="sports"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                setProgress={setProgress}
+                key="technology"
+                image={URL}
+                apiKey={apiKey}
+                pageSize={pageSize}
+                country={country}
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+export default App;
--- src/components/News.js
+++ src/components/News.js
@@ -1,125 +1,120 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewBar from "./NewsBar";
 import Spinner from "./Spinner";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import PropTypes from "prop-types";
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 20,
-    category: "general",
-  };
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-  capitalize = (string) => {
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setState] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults: 0,
-    };
-    document.title = `NewsFlash - ${this.capitalize(this.props.category)}`;
-  }
-  async updateNews() {
-    this.props.setProgress(10);
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    this.props.setProgress(30);
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.props.setProgress(60);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-    this.props.setProgress(100);
-  }
-  async componentDidMount() {
-    this.updateNews();
-  }
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-  };
-  render() {
-    return (
-      <>
-        <h3
-          style={{
-            textAlign: "center",
-            fontFamily: "monospace",
-            marginTop: "4rem",
-          }}
-        >
-          TOP{" "}
-          <span style={{ color: "red", fontWeight: "bold" }}>
-            {this.capitalize(this.props.category)}
-          </span>{" "}
-          Headlines Today
-        </h3>
 
-        {this.state.loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-3 my-3" key={element.url}>
-                    <NewBar
-                      title={element.title === null ? "NIL" : element.title}
-                      description={
-                        element.description === null
-                          ? "NIL"
-                          : element.description.length > 0
-                          ? element.description.length > 90
-                            ? element.description.slice(0, 90) + "..."
-                            : element.description
-                          : "NIL"
-                      }
-                      // title={element.title}
-                      // description={element.description}
-                      imageURL={
-                        element.urlToImage === null
-                          ? "https://user-images.githubusercontent.com/89683890/195827237-a0ffe9dc-6569-4179-8ae4-8ee4e4edc044.jpg"
-                          : element.urlToImage
-                      }
-                      newsURL={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+  const updateNews = async () => {
+    props.setProgress(10);
+    setState(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+
+    let parsedData = await data.json();
+    props.setProgress(60);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
+  useEffect(() => {
+    document.title = `NewsFlash - ${capitalize(props.category)}`;
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
+  const fetchMoreData = async () => {
+    setState(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+  };
+
+  return (
+    <>
+      <h3
+        style={{
+          textAlign: "center",
+          fontFamily: "monospace",
+          marginTop: "4rem",
+        }}
+      >
+        TOP{" "}
+        <span style={{ color: "red", fontWeight: "bold" }}>
+          {capitalize(props.category)}
+        </span>{" "}
+        Headlines Today
+      </h3>
+
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-3 my-3" key={element.url}>
+                  <NewBar
+                    title={element.title === null ? "NIL" : element.title}
+                    description={
+                      element.description === null
+                        ? "NIL"
+                        : element.description.length > 0
+                        ? element.description.length > 90
+                          ? element.description.slice(0, 90) + "..."
+                          : element.description
+                        : "NIL"
+                    }
+                    // title={element.title}
+                    // description={element.description}
+                    imageURL={
+                      element.urlToImage === null
+                        ? "https://user-images.githubusercontent.com/89683890/195827237-a0ffe9dc-6569-4179-8ae4-8ee4e4edc044.jpg"
+                        : element.urlToImage
+                    }
+                    newsURL={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
-}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 20,
+  category: "general",
+};
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
 
@@ -129,3 +124,6 @@ export default News;
 //au->australia us-> united states nz-> new Zealand ca->canada sg->singapore ph ->Philippines sa->south Africa my->Malaysia
 //884d2958a8264bf09e3fe2044580c419
 //04fb5ddc408845a7864df9dbb85d6db8
+//ae ar at au be bg br ca ch cn co cu cz de eg fr gb
+//gr hk hu id ie il in it jp kr lt lv ma mx my ng nl
+//no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za
